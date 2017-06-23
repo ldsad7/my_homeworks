8fe1d8a9cb07557d84a75ac13fb2205828e22d0b
Eduard <happypuffin7@gmail.com>
2017-06-23 13:51:37 +0300

    Add files via upload

--- cw3/q.py
+++ cw3/q.py
@@ -2,7 +2,7 @@ import re
 import os
 import csv
 
-def printing(d1, d2):
+def printing(d1, d2, arr):
     f = open('output1.txt', 'w', encoding = 'cp1251')
     for key, value in sorted(d1.items()):
         f.write(key + '\t' + str(value) + '\n')
@@ -13,7 +13,10 @@ def printing(d1, d2):
         for key, value in sorted(d2.items()):
             lst = [str(key), str(value[0]), str(value[1])]
             writer.writerow(lst)
-    
+    f = open('output3.txt', 'w', encoding = 'cp1251')
+    for elem in arr:
+        f.write(elem + '\n')
+    f.close()
 
 def dictionary(name):
     d1 = {}
@@ -27,16 +30,37 @@ def dictionary(name):
             b = re.findall('<meta content="(.*?)" name="author"', text)
             c = re.findall('<meta content="(.*?)" name="created"', text)
             d2[file] = b + c
-            words = re.findall('<w><.*?>(.*?)</w>', text)
+            words_punct = re.findall('<w><.*?></ana>(.*?)</w>([\s,.!123456790:;?""])', text)
+            words = [words_punct[i][0] for i in range(len(words_punct))]
+            puncts = [words_punct[i][1] for i in range(len(words_punct))]
             d = re.findall('gr="A.*?gen.*?></ana>(.*?)</w>\s<w>.*?gr="S.*?gen.*?></ana>(.*?)</w>', text)
-            arr.extend(d)
-    print(arr)
+            e = []
+            for i, elem in enumerate(d):
+                ind1 = words.index(elem[0])
+                ind2 = words.index(elem[1])
+                if ind2 - ind1 == 1:
+                    t = ind1 - 1
+                    while t >= 0 and puncts[t] not in '[.?!]':
+                        t -= 1
+                    k = ind2
+                    while k <= len(words) - 1 and puncts[k] not in '[.?!]':
+                        k += 1
+                    s = ''
+                    for p in range(t + 1, k):
+                        if p != ind1 and p != ind2:
+                            s += words[p] + puncts[p]
+                        elif p == ind1:
+                            s += '\t' + words[p] + puncts[p]
+                        else:
+                            s += words[p] + puncts[p] + 't'
+                    e.append(s)
+            arr.extend(e)
     return d1, d2, arr
 
 def main():
-    d1, d2, d3 = dictionary('news')
+    d1, d2, arr = dictionary('news')
     
-    printing(d1, d2)
+    printing(d1, d2, arr)
     
 
 if __name__ == '__main__':
